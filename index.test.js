5892ed2fea9c58a806b8e46f20530ac21b4bc602
Fredrik Andersson <f.anderzon@gmail.com>
2017-09-03 12:43:07 +0200

    Added test for mapper function

--- index.test.js
+++ index.test.js
@@ -1,11 +1,23 @@
 import chainPromiseData from './index';
 
+const user = { id: 1, name: 'Chainy McChainFace'};
+
 function createUser() {
-  return Promise.resolve({ id: 1, name: 'Chainy McChainFace'});
+  return Promise.resolve(user);
 }
 
 describe('chainPromiseData', () => {
   it('Should return a function', () => {
     expect(typeof chainPromiseData(createUser)).toBe('function');
+  });
+  it('Should apply a mapper function if provided', () => {
+    chainPromiseData(
+      createUser,
+      ({ id, name }) => ({ userId: id, userName: name })
+    )()
+      .then(data => {
+        expect(data.userId).toBe(user.id);
+        expect(data.userName).toBe(user.name);
+      })
   });
 });
