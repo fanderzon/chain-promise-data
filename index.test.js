3f93e4dce677e37e62b4165bf7eebb5f2ef1abfe
Fredrik Andersson <f.anderzon@gmail.com>
2017-09-03 12:51:16 +0200

    Added test for actually chaining the data

--- index.test.js
+++ index.test.js
@@ -1,9 +1,18 @@
 import chainPromiseData from './index';
 
 const user = { id: 1, name: 'Chainy McChainFace'};
+const food = { food: 'Burger' };
 
 function createUser() {
   return Promise.resolve(user);
+}
+
+function makeFood() {
+  return Promise.resolve(food);
+}
+
+function returnsNothing() {
+  return Promise.resolve();
 }
 
 describe('chainPromiseData', () => {
@@ -29,5 +38,15 @@ describe('chainPromiseData', () => {
         expect(data.id).toBeUndefined();
         expect(data.name).toBe(user.name);
       });
-  })
+  });
+  it('Should hold on to data through longer chains', () => {
+    createUser()
+      .then(chainPromiseData(makeFood))
+      .then(chainPromiseData(returnsNothing))
+      .then(data => {
+        expect(data.id).toBe(user.id);
+        expect(data.name).toBe(user.name);
+        expect(data.food).toBe(food.food);
+      })
+  });
 });
